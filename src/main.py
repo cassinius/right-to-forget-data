bdcdc1c97779da83c1939a46224986bc524532bd
Bernd Malle <bernd.malle@gmail.com>
2016-06-29 04:23:18 +0200

    added nodeID row plus indices to all files and computing them in a row now into a ready-to-use results dataset

--- src/main.py
+++ src/main.py
@@ -13,173 +13,181 @@ import sklearn.tree as tree
 import seaborn as sns
 import matplotlib.pyplot as plt
 import math
+import os, csv, glob
 
 
-INPUT_FILE = '../data/adults_sanitized.csv'
-INPUT_ANON_K7_EQUAL = '../data/adults_anonymized_k7.csv'
-INPUT_ANON_K7_RACE = '../data/adults_anonymized_k7_race_important.csv'
-INPUT_ANON_K7_AGE = '../data/adults_anonymized_k7_age_important.csv'
-INPUT_ANON_K13_EQUAL = '../data/adults_anonymized_k13.csv'
-INPUT_ANON_K13_RACE = '../data/adults_anonymized_k13_race_important.csv'
-INPUT_ANON_K13_AGE = '../data/adults_anonymized_k13_age_important.csv'
-INPUT_ANON_K19_EQUAL = '../data/adults_anonymized_k19.csv'
-INPUT_ANON_K19_RACE = '../data/adults_anonymized_k19_race_important.csv'
-INPUT_ANON_K19_AGE = '../data/adults_anonymized_k19_age_important.csv'
-INPUT_ANON_K25_EQUAL = '../data/adults_anonymized_k25.csv'
-INPUT_ANON_K25_RACE = '../data/adults_anonymized_k25_race_important.csv'
-INPUT_ANON_K25_AGE = '../data/adults_anonymized_k25_age_important.csv'
-INPUT_ANON_K31_EQUAL = '../data/adults_anonymized_k31.csv'
-INPUT_ANON_K31_RACE = '../data/adults_anonymized_k31_race_important.csv'
-INPUT_ANON_K31_AGE = '../data/adults_anonymized_k31_age_important.csv'
-INPUT_SELECTED_DELETE_UNITED_STATES = '../data/adults_marital-status_Married-civ-spouse_0.1.csv'
+INPUT_CSV = '../data/'
+OUTPUT_CSV = '../output/'
 
 
-original_data = pd.read_csv(
-    INPUT_SELECTED_DELETE_UNITED_STATES,
-    names = [
-        "nodeID", "age", "workclass", "native-country", "sex", "race", "marital-status", "income" # "nodeID",
-    ],
-    header=0,
-    index_col=0,
-    sep=r'\s*,\s*',
-    engine='python',
-    na_values="?")
-# print original_data.tail()
+def runLogisticRegression(input_file):
+
+  original_data = pd.read_csv(
+      input_file,
+      names = [
+          "nodeID", "age", "workclass", "native-country", "sex", "race", "marital-status", "income"
+      ],
+      header=0,
+      index_col=0,
+      sep=r'\s*,\s*',
+      engine='python',
+      na_values="?")
+  # print original_data.tail()
 
 
-fig = plt.figure(figsize=(20,15))
-cols = 3
-rows = math.ceil(float(original_data.shape[1]) / cols)
-for i, column in enumerate(original_data.columns):
-    if i == len(original_data.columns) - 1:
-        break
-    ax = fig.add_subplot(rows, cols, i + 1)
-    ax.set_title(column)
-    if original_data.dtypes[column] == np.object:
-        original_data[column].value_counts().plot(kind="bar", axes=ax)
-    else:
-        original_data[column].hist(axes=ax)
+  fig = plt.figure(figsize=(20,15))
+  cols = 3
+  rows = math.ceil(float(original_data.shape[1]) / cols)
+  for i, column in enumerate(original_data.columns):
+      if i == len(original_data.columns) - 1:
+          break
+      ax = fig.add_subplot(rows, cols, i + 1)
+      ax.set_title(column)
+      if original_data.dtypes[column] == np.object:
+          original_data[column].value_counts().plot(kind="bar", axes=ax)
+      else:
+          original_data[column].hist(axes=ax)
 
-    plt.xticks(rotation=55)
+      plt.xticks(rotation=55)
 
-plt.subplots_adjust(top=0.9, bottom=0, hspace=0.4, wspace=0.2)
-# plt.show()
-
-
-# print (original_data["native-country"].value_counts() / original_data.shape[0]).head()
+  plt.subplots_adjust(top=0.9, bottom=0, hspace=0.4, wspace=0.2)
+  # plt.show()
 
 
-# Encode the categorical features as numbers
-def number_encode_features(df):
-    result = df.copy()
-    encoders = {}
-    for column in result.columns:
-        if result.dtypes[column] == np.object:
-            encoders[column] = preprocessing.LabelEncoder()
-            result[column] = encoders[column].fit_transform(result[column])
-
-    return result, encoders
+  # print (original_data["native-country"].value_counts() / original_data.shape[0]).head()
 
 
-# PLOT CORRELATION OF ORIGINAL FEATURES
-plt.subplots(figsize=(10,10))
-# Calculate the correlation and plot it
-encoded_data, _ = number_encode_features(original_data)
-plt.xticks(rotation=90)
-plt.yticks(rotation=0)
-sns.heatmap(encoded_data.corr(), square=True)
-# plt.show()
+  # Encode the categorical features as numbers
+  def number_encode_features(df):
+      result = df.copy()
+      encoders = {}
+      for column in result.columns:
+          if result.dtypes[column] == np.object:
+              encoders[column] = preprocessing.LabelEncoder()
+              result[column] = encoders[column].fit_transform(result[column])
+
+      return result, encoders
 
 
-# ENCODE FEATURES AS NUMBERS
-encoded_data, encoders = number_encode_features(original_data)
-fig = plt.figure(figsize=(20,15))
-cols = 3
-rows = math.ceil(float(encoded_data.shape[1]) / cols)
-for i, column in enumerate(encoded_data.columns):
-    if i == len(encoded_data.columns) - 1:
-        break
-    ax = fig.add_subplot(rows, cols, i + 1)
-    ax.set_title(column)
-    encoded_data[column].hist(axes=ax)
-    plt.xticks(rotation="vertical")
-plt.subplots_adjust(hspace=0.2, wspace=0.2)
-# plt.show()
+  # PLOT CORRELATION OF ORIGINAL FEATURES
+  plt.subplots(figsize=(10,10))
+  # Calculate the correlation and plot it
+  encoded_data, _ = number_encode_features(original_data)
+  plt.xticks(rotation=90)
+  plt.yticks(rotation=0)
+  sns.heatmap(encoded_data.corr(), square=True)
+  # plt.show()
 
 
-# TRAIN A LOGISTIC REGRESSION MODEL
-X_train, X_test, y_train, y_test = cross_validation.train_test_split(encoded_data[encoded_data.columns - ["income"]], encoded_data["income"], train_size=0.80)
-scaler = preprocessing.StandardScaler()
-X_train = pd.DataFrame(scaler.fit_transform(X_train.astype("float64")), columns=X_train.columns)
-X_test = scaler.transform(X_test.astype("float64"))
+  # ENCODE FEATURES AS NUMBERS
+  encoded_data, encoders = number_encode_features(original_data)
+  fig = plt.figure(figsize=(20,15))
+  cols = 3
+  rows = math.ceil(float(encoded_data.shape[1]) / cols)
+  for i, column in enumerate(encoded_data.columns):
+      if i == len(encoded_data.columns) - 1:
+          break
+      ax = fig.add_subplot(rows, cols, i + 1)
+      ax.set_title(column)
+      encoded_data[column].hist(axes=ax)
+      plt.xticks(rotation="vertical")
+  plt.subplots_adjust(hspace=0.2, wspace=0.2)
+  # plt.show()
 
 
-# LOGISTIC REGRESSION
-cls = linear_model.LogisticRegression()
-
-cls.fit(X_train, y_train)
-y_pred = cls.predict(X_test)
-cm = metrics.confusion_matrix(y_test, y_pred)
-plt.figure(figsize=(12,12))
-plt.subplot(2,1,1)
-sns.heatmap(cm, annot=True, fmt="d", xticklabels=encoders["income"].classes_, yticklabels=encoders["income"].classes_)
-plt.ylabel("Real value")
-plt.xlabel("Predicted value")
-
-# print "Precision: %f" % skl.metrics.precision_score(y_test, y_pred)
-# print "Recall: %f" % skl.metrics.recall_score(y_test, y_pred)
-# print "F1 score: %f" % skl.metrics.f1_score(y_test, y_pred)
-
-coefs = pd.Series(cls.coef_[0], index=X_train.columns)
-coefs.sort()
-plt.subplot(2,1,2)
-coefs.plot(kind="bar")
-# plt.show()
+  # TRAIN A LOGISTIC REGRESSION MODEL
+  X_train, X_test, y_train, y_test = cross_validation.train_test_split(encoded_data[encoded_data.columns - ["income"]], encoded_data["income"], train_size=0.80)
+  scaler = preprocessing.StandardScaler()
+  X_train = pd.DataFrame(scaler.fit_transform(X_train.astype("float64")), columns=X_train.columns)
+  X_test = scaler.transform(X_test.astype("float64"))
 
 
-# Binary features
-binary_data = pd.get_dummies(original_data)
-# Let's fix the Target as it will be converted to dummy vars too
-binary_data["income"] = binary_data["income_>50K"]
-del binary_data["income_<=50K"]
-del binary_data["income_>50K"]
-plt.subplots(figsize=(20,20))
-sns.heatmap(binary_data.corr(), square=True)
+  # LOGISTIC REGRESSION
+  cls = linear_model.LogisticRegression()
 
-plt.xticks(rotation=90)
-plt.yticks(rotation=0)
-# plt.show()
+  cls.fit(X_train, y_train)
+  y_pred = cls.predict(X_test)
+  cm = metrics.confusion_matrix(y_test, y_pred)
+  plt.figure(figsize=(12,12))
+  plt.subplot(2,1,1)
+  sns.heatmap(cm, annot=True, fmt="d", xticklabels=encoders["income"].classes_, yticklabels=encoders["income"].classes_)
+  plt.ylabel("Real value")
+  plt.xlabel("Predicted value")
 
+  # print "Precision: %f" % skl.metrics.precision_score(y_test, y_pred)
+  # print "Recall: %f" % skl.metrics.recall_score(y_test, y_pred)
+  # print "F1 score: %f" % skl.metrics.f1_score(y_test, y_pred)
 
-# Use binary for Logistic regression
-X_train, X_test, y_train, y_test = cross_validation.train_test_split(binary_data[binary_data.columns - ["income"]], binary_data["income"], train_size=0.80)
-scaler = preprocessing.StandardScaler()
-X_train = pd.DataFrame(scaler.fit_transform(X_train), columns=X_train.columns)
-X_test = scaler.transform(X_test)
-
-
-# Plot logistic regression again
-cls = linear_model.LogisticRegression()
-cls.fit(X_train, y_train)
-y_pred = cls.predict(X_test)
-cm = metrics.confusion_matrix(y_test, y_pred)
-plt.figure(figsize=(20,20))
-plt.subplot(2,1,1)
-sns.heatmap(cm, annot=True, fmt="d", xticklabels=encoders["income"].classes_, yticklabels=encoders["income"].classes_)
-plt.ylabel("Real value")
-plt.xlabel("Predicted value")
-# plt.show()
+  coefs = pd.Series(cls.coef_[0], index=X_train.columns)
+  coefs.sort()
+  plt.subplot(2,1,2)
+  coefs.plot(kind="bar")
+  # plt.show()
 
 
-print "Precision: %f" % skl.metrics.precision_score(y_test, y_pred)
-print "Recall: %f" % skl.metrics.recall_score(y_test, y_pred)
-print "F1 score: %f" % skl.metrics.f1_score(y_test, y_pred)
+  # Binary features
+  binary_data = pd.get_dummies(original_data)
+  # Let's fix the Target as it will be converted to dummy vars too
+  binary_data["income"] = binary_data["income_>50K"]
+  del binary_data["income_<=50K"]
+  del binary_data["income_>50K"]
+  plt.subplots(figsize=(20,20))
+  sns.heatmap(binary_data.corr(), square=True)
+
+  plt.xticks(rotation=90)
+  plt.yticks(rotation=0)
+  # plt.show()
 
 
+  # Use binary for Logistic regression
+  X_train, X_test, y_train, y_test = cross_validation.train_test_split(binary_data[binary_data.columns - ["income"]], binary_data["income"], train_size=0.80)
+  scaler = preprocessing.StandardScaler()
+  X_train = pd.DataFrame(scaler.fit_transform(X_train), columns=X_train.columns)
+  X_test = scaler.transform(X_test)
 
-coefs = pd.Series(cls.coef_[0], index=X_train.columns)
-coefs.sort()
-ax = plt.subplot(2,1,1)
-coefs.plot(kind="bar", rot=90, figsize=(5, 7))
 
-# plt.show()
+  # Plot logistic regression again
+  cls = linear_model.LogisticRegression()
+  cls.fit(X_train, y_train)
+  y_pred = cls.predict(X_test)
+  cm = metrics.confusion_matrix(y_test, y_pred)
+  plt.figure(figsize=(20,20))
+  plt.subplot(2,1,1)
+  sns.heatmap(cm, annot=True, fmt="d", xticklabels=encoders["income"].classes_, yticklabels=encoders["income"].classes_)
+  plt.ylabel("Real value")
+  plt.xlabel("Predicted value")
+  # plt.show()
+
+
+  coefs = pd.Series(cls.coef_[0], index=X_train.columns)
+  coefs.sort()
+  ax = plt.subplot(2,1,1)
+  coefs.plot(kind="bar", rot=90, figsize=(5, 7))
+
+  # plt.show()
+
+
+  precision = skl.metrics.precision_score(y_test, y_pred)
+  # print "Precision: %f" % precision
+  recall = skl.metrics.recall_score(y_test, y_pred)
+  # print "Recall: %f" % recall
+  f1 = skl.metrics.f1_score(y_test, y_pred)
+  # print "F1 score: %f" % f1
+
+  return [precision, recall, f1]
+
+
+# COMPILING RESULTS
+filelist = [ f for f in sorted(os.listdir(INPUT_CSV)) if f.endswith(".csv") ]
+
+with open(OUTPUT_CSV + "results.csv", 'wb') as fout:
+  writer = csv.writer(fout, lineterminator='\n')
+  writer.writerow(["dataset", "precision", "recall", "F1 score"])
+
+  for input_file in filelist:
+    # print input_file
+    scores = runLogisticRegression(INPUT_CSV + input_file)
+    scores.insert(0, input_file)
+    writer.writerow(scores)
+    print scores
+
