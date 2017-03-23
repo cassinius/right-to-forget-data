3a5240356af6b19a91d15ca5aeb1e3bc04fb65a0
Bernd Malle <bernd.malle@gmail.com>
2017-03-23 23:11:41 +0100

    added gradient boosting results for education-num - first result that matches expectations...

--- src/binary_class/plotting.py
+++ src/binary_class/plotting.py
@@ -13,19 +13,19 @@ from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 
 
 # Adults education-num files
-# gradient_boost_file = '../../output/adults_target_education_num/results_gradient_boosting.csv'
-# logistic_regression_file = '../../output/adults_target_education_num/results_logistic_regression.csv'
-# onevsrest_bagging_file = '../../output/adults_target_education_num/results_onevsrest_bagging.csv'
-# random_forest_file = '../../output/adults_target_education_num/results_random_forest.csv'
-# linear_svc_file = '../../output/adults_target_education_num/results_linear_svc.csv'
+gradient_boost_file = '../../output/adults_target_education_num/results_gradient_boosting.csv'
+logistic_regression_file = '../../output/adults_target_education_num/results_logistic_regression.csv'
+onevsrest_bagging_file = '../../output/adults_target_education_num/results_onevsrest_bagging.csv'
+random_forest_file = '../../output/adults_target_education_num/results_random_forest.csv'
+linear_svc_file = '../../output/adults_target_education_num/results_linear_svc.csv'
 
 
 # Adults marital-status files
-gradient_boost_file = '../../output/adults_target_marital_status/results_gradient_boosting.csv'
-logistic_regression_file = '../../output/adults_target_marital_status/results_logistic_regression.csv'
-onevsrest_bagging_file = '../../output/adults_target_marital_status/results_onevsrest_bagging.csv'
-random_forest_file = '../../output/adults_target_marital_status/results_random_forest.csv'
-linear_svc_file = '../../output/adults_target_marital_status/results_linear_svc.csv'
+# gradient_boost_file = '../../output/adults_target_marital_status/results_gradient_boosting.csv'
+# logistic_regression_file = '../../output/adults_target_marital_status/results_logistic_regression.csv'
+# onevsrest_bagging_file = '../../output/adults_target_marital_status/results_onevsrest_bagging.csv'
+# random_forest_file = '../../output/adults_target_marital_status/results_random_forest.csv'
+# linear_svc_file = '../../output/adults_target_marital_status/results_linear_svc.csv'
 
 
 def readResultsIntoHash(file_name):
@@ -77,7 +77,7 @@ def plotAnonymizationResults(results):
   rect = fig.patch
   rect.set_facecolor('white')
 
-  title_algo = "Random Forest"
+  title_algo = "Gradient Boost"
   plt.title("F1 score dependent on k-factor, %s" % (title_algo) )
 
   equal_line, = plt.plot(equal_line_f1, marker='o', linestyle='-', color='r', label="equal weights")
@@ -198,7 +198,7 @@ if __name__ == "__main__":
   algorithm = "random_forest"
   target = "education_num"
 
-  results = readResultsIntoHash(random_forest_file)
+  results = readResultsIntoHash(gradient_boost_file)
   plotAnonymizationResults(results)
   # plotPerturbationResultsTop3(results)
   # plotPerturbationResultsBottom3(results)
