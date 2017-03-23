2388be9318ad959e5567d6b41abe06982fb991c3
Bernd Malle <bernd.malle@gmail.com>
2017-03-24 00:20:28 +0100

    re-ran linear svc on extended datasets and added performance figures

--- src/binary_class/plotting.py
+++ src/binary_class/plotting.py
@@ -78,7 +78,7 @@ def plotAnonymizationResults(results):
   rect = fig.patch
   rect.set_facecolor('white')
 
-  title_algo = "Gradient Boost"
+  title_algo = "Linear SVC"
   plt.title("F1 score dependent on k-factor, %s" % (title_algo) )
 
   equal_line, = plt.plot(equal_line_f1, marker='o', linestyle='-', color='r', label="equal weights")
@@ -199,7 +199,7 @@ if __name__ == "__main__":
   algorithm = "random_forest"
   target = "education_num"
 
-  results = readResultsIntoHash(gradient_boost_file)
+  results = readResultsIntoHash(linear_svc_file)
   plotAnonymizationResults(results)
   # plotPerturbationResultsTop3(results)
   # plotPerturbationResultsBottom3(results)
