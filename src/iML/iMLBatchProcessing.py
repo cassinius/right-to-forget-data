1e454b69e0685591b34d7f6dac38a15a350e4af4
Bernd Malle <bernd.malle@gmail.com>
2017-07-25 21:58:33 +0200

    wrote code for batch classification of all accumulated / anonymized iml-based data

--- src/iML/iMLBatchProcessing.py
+++ src/iML/iMLBatchProcessing.py
@@ -12,28 +12,24 @@ ORIGINAL_DATA_FILE = "original_data_5000_rows.csv"
 
 CROSS_VALIDATION_K = 10
 
-CONFIG_EDUCATION = {
-    'INPUT_COLS': [
+INPUT_COLS = {
+    "education-num": [
         "age",
         "hours-per-week",
         "workclass",
         "native-country",
         "sex",
         "race",
-        "marital-status",
         "relationship",
         "occupation",
+        "marital-status",
         "income",
         "education-num"
-     ],
-    'TARGET_COL': "education-num"
-}
-
-CONFIG_MARITAL = {
-    'INPUT_COLS': [
+    ],
+    "marital-status": [
         "age",
-        "education-num",
         "hours-per-week",
+        "education-num",
         "workclass",
         "native-country",
         "sex",
@@ -42,27 +38,21 @@ CONFIG_MARITAL = {
         "occupation",
         "income",
         "marital-status"
-     ],
-    'TARGET_COL': "marital-status"
-}
-
-CONFIG_INCOME = {
-    'INPUT_COLS': [
+    ],
+    "income": [
         "age",
-        "education-num",
         "hours-per-week",
+        "education-num",
         "workclass",
         "native-country",
         "sex",
         "race",
-        "marital-status",
         "relationship",
         "occupation",
+        "marital-status",
         "income"
-     ],
-    'TARGET_COL': "income"
+    ]
 }
-
 
 ALGORITHMS = [
     'linear_svc',
@@ -80,32 +70,68 @@ def main_workflow():
         algorithm = importlib.import_module('src.multi_class.' + algo_str)
 
         with open(OUTPUT_DIR + 'results_' + algo_str + "_equal.csv", 'w') as equal_out, \
-             open(OUTPUT_DIR + 'results_' + algo_str + "_bias.csv", 'w') as bias_out, \
-             open(OUTPUT_DIR + 'results_' + algo_str + "_iml.csv", 'w') as iml_out:
+                open(OUTPUT_DIR + 'results_' + algo_str + "_bias.csv", 'w') as bias_out, \
+                open(OUTPUT_DIR + 'results_' + algo_str + "_iml.csv", 'w') as iml_out:
 
             equal_writer = csv.writer(equal_out, lineterminator='\n')
             bias_writer = csv.writer(bias_out, lineterminator='\n')
             iml_writer = csv.writer(iml_out, lineterminator='\n')
 
-            equal_writer.writerow(["k-factor", "f1_score"])
-            bias_writer.writerow(["k-factor", "f1_score"])
-            iml_writer.writerow(["k-factor", "f1_score"])
+            equal_writer.writerow(["target", "k-factor", "f1_score"])
+            bias_writer.writerow(["target", "k-factor", "f1_score"])
+            iml_writer.writerow(["target", "k-factor", "f1_score"])
 
             for input_file in filelist:
-                # Split filename to see what we're dealing with here...
-                if ( input_file == ORIGINAL_DATA_FILE ):
+                # We only need the original result once, which we will hardcode somewhere..
+                if input_file == ORIGINAL_DATA_FILE:
                     continue
-                print(input_file)
 
+                # Split filename to see what we're dealing with here...
+                file_components = input_file.split('_')
+                k_factor = file_components[len(file_components)-1].split('.')[0]
+                weight_category = file_components[len(file_components)-2]
+                target = file_components[len(file_components)-3]
+                print(weight_category)
+                print(target)
+                print(k_factor)
 
+                encoded_data = input_preproc.readFromDataset(
+                    INPUT_DIR + input_file,
+                    INPUT_COLS[target],
+                    target
+                )
 
+                # Split into predictors and target
+                X = np.array(encoded_data[encoded_data.columns.difference([target])])
+                y = np.array(encoded_data[target])
+                kf = KFold(n_splits=CROSS_VALIDATION_K, shuffle=True)
 
+                f1s = []
 
+                for train_index, test_index in kf.split(X):
+                    X_train, y_train = X[train_index], y[train_index]
+                    X_test, y_test = X[test_index], y[test_index]
 
+                    scaler = preprocessing.StandardScaler()
+                    X_train = pd.DataFrame(scaler.fit_transform(X_train))  # , columns=X_train.columns)
+                    X_test = scaler.transform(X_test)
 
+                    precision, recall, f1_score, accuracy = algorithm.runClassifier(X_train, X_test, y_train, y_test)
+                    f1s.append(f1_score)
 
+                final_f1 = sum(f1s) / len(f1s)
+                print("\n================================")
+                print("F1 Score: %.6f" % (final_f1))
+                print("================================\n")
 
-
+                if weight_category == 'equal':
+                    equal_writer.writerow([target, k_factor, final_f1])
+                elif weight_category == 'bias':
+                    bias_writer.writerow([target, k_factor, final_f1])
+                elif weight_category == 'iml':
+                    iml_writer.writerow([target, k_factor, final_f1])
+                else:
+                    raise Exception("Unknown weight category. Are you overweight???")
 
 if __name__ == "__main__":
   main_workflow()
